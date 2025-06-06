0ad7cdc4e0023eb4ce82c141af58160e5d0afa95
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2025-06-07 04:34:05 +0500

    Project SetUp and Installing few Initiallizing Packages

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from 'next';
+import React from 'react'
+
+
+export const metadata: Metadata = {
+  title: "StoreIt",
+  description: "StoreIt - The only storage solution you need.",
+};
+
+const RootLayout = () => {
+  return (
+    <div>RootLayout</div>
+  )
+}
+
+export default RootLayout;
---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>
+        Storeit - The only storage solution you need
+      </h1>
+    </main>
+  );
+}
