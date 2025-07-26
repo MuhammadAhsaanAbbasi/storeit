62d47b4e92bd6bfaa4c41eb30a67edf214161160
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2025-07-26 20:37:12 +0500

    Layout Nav, Sidebar

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,3 +1,6 @@
+import Header from '@/components/navigation/Header';
+import MobileNav from '@/components/navigation/MobileNav';
+import SideBar from '@/components/navigation/SideBar';
 import { Metadata } from 'next';
 import React, { ReactNode } from 'react'
 
@@ -6,10 +9,17 @@ export const metadata: Metadata = {
   description: "StoreIt - The only storage solution you need.",
 };
 
-const RootLayout = ({children}:{children: ReactNode}) => {
+const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <main className='font-poppins'>
-        {children}
+    <main className='flex h-screen font-poppins'>
+      <SideBar />
+      <section className='flex h-full flex-1 flex-col'>
+        <Header />
+        <MobileNav />
+        <div className='main-content'>
+          {children}
+        </div>
+      </section>
     </main>
   )
 }
---
+++ src/components/navigation/SideBar.tsx
@@ -0,0 +1,70 @@
+"use client";
+import { navItems } from '@/constants';
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react'
+
+const SideBar = () => {
+  const pathname = usePathname();
+  return (
+    <aside className='sidebar'>
+      <Link href={"/"}>
+        <Image
+          src={"/icons/logo-full-brand.svg"}
+          alt="logo"
+          width={160}
+          height={60}
+          className='hidden h-auto lg:block'
+        />
+
+        <Image
+          src={"/icons/logo-brand.svg"}
+          alt="logo"
+          width={60}
+          height={60}
+          className='lg:hidden'
+        />
+      </Link>
+
+      {/* Navigation Links */}
+      <nav className='sidebar-nav'>
+        <ul className='flex flex-1 flex-col gap-6'>
+          {
+            navItems.map(({ url, icon, name }, index) => {
+              const path = url === pathname;
+              return (
+                <Link key={index} href={url}
+                  className='lg:w-full'>
+                  <li
+                    className={cn(
+                      "sidebar-nav-item",
+                      path && "shad-active",
+                    )}
+                  >
+                    <Image
+                      src={icon}
+                      alt={name}
+                      width={24}
+                      height={24}
+                      className={cn(
+                        "nav-icon",
+                        path && "nav-icon-active",
+                      )}
+                    />
+                    <p className='hidden lg:block'>
+                      {name}
+                    </p>
+                  </li>
+                </Link>
+              )
+            })
+          }
+        </ul>
+      </nav>
+    </aside>
+  )
+}
+
+export default SideBar;
