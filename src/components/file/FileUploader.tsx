e1a279979b00e861ae026eb678c055b522017d18
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2025-08-03 22:40:59 +0500

    File Uploader Button

--- src/components/file/FileUploader.tsx
+++ src/components/file/FileUploader.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+"use client"
+import React, { useCallback } from 'react'
+import { useDropzone } from 'react-dropzone'
+import { Button } from '../ui/button'
+import Image from 'next/image'
 
 const FileUploader = () => {
+    const onDrop = useCallback((acceptedFiles: File[]) => {
+        // Do something with the files
+    }, [])
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
     return (
-        <div>FileUploader</div>
+        <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            <Button className='uploader-button'>
+                <Image
+                    src={"/icons/upload.svg"}
+                    alt="upload"
+                    width={24}
+                    height={24}
+                />
+                <span className='body-1'>
+                    Upload
+                </span>
+            </Button>
+        </div>
     )
 }
 
