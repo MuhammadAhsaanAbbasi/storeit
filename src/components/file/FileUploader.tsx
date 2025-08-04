ae9f048a9821ac428d5ef20daa51a6a1155e2019
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2025-08-04 10:39:22 +0500

    File Uploader Button UI, Backend is not integerated

--- src/components/file/FileUploader.tsx
+++ src/components/file/FileUploader.tsx
@@ -1,18 +1,31 @@
 "use client"
-import React, { useCallback } from 'react'
+import React, { useCallback, useState } from 'react'
 import { useDropzone } from 'react-dropzone'
 import { Button } from '../ui/button'
 import Image from 'next/image'
+import { cn, convertFileToUrl, getFileIcon, getFileType } from '@/lib/utils'
+import Thumbnail from './Thumbnail'
 
-const FileUploader = () => {
+
+interface IProps {
+    className: string;
+}
+
+const FileUploader = ({ className }: IProps) => {
+    const [files, setFiles] = useState<File[]>([]);
     const onDrop = useCallback((acceptedFiles: File[]) => {
-        // Do something with the files
+        setFiles(acceptedFiles);
     }, [])
-    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+    const handleRemoveFile = (e: React.MouseEvent<HTMLDivElement, MouseEvent>, fileName: string) => {
+        e.stopPropagation();
+        setFiles((prev) => prev.filter((file) => file.name !== fileName))
+    }
     return (
-        <div {...getRootProps()}>
+        <div {...getRootProps()} className="cursor-pointer">
             <input {...getInputProps()} />
-            <Button className='uploader-button'>
+            <Button type="button" className={cn("uploader-button", className)}>
                 <Image
                     src={"/icons/upload.svg"}
                     alt="upload"
@@ -23,6 +36,51 @@ const FileUploader = () => {
                     Upload
                 </span>
             </Button>
+            {
+                files.length > 0 && (
+                    <ul className='uploader-preview-list'>
+                        <h4 className="h4 text-light-100">Uploading</h4>
+                        {
+                            files.map((file, index) => {
+                                const { type, extension } = getFileType(file.name);
+                                return (
+                                    <li key={index} className='uploader-preview-item'>
+                                        <div className='flex items-center gap-4'>
+                                            <Thumbnail
+                                                type={type}
+                                                extension={extension}
+                                                url={convertFileToUrl(file)}
+                                            />
+                                            <div className="preview-item-name">
+                                                {file.name}
+                                                {/* <span>
+                                                </span> */}
+                                                <Image
+                                                    src="/icons/file-loader.gif"
+                                                    width={80}
+                                                    height={26}
+                                                    alt="Loader"
+                                                    unoptimized={true}
+                                                />
+                                            </div>
+                                        </div>
+                                        <div className='cursor-pointer'
+                                        onClick={(e) => handleRemoveFile(e, file.name)}
+                                        >
+                                            <Image
+                                                src={"/icons/remove.svg"}
+                                                width={24}
+                                                height={24}
+                                                alt="Remove"
+                                            />
+                                        </div>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                )
+            }
         </div>
     )
 }
---
+++ src/components/file/Thumbnail.tsx
@@ -0,0 +1,31 @@
+import { cn, getFileIcon } from '@/lib/utils';
+import Image from 'next/image';
+import React from 'react'
+
+interface IProps {
+    type: string;
+    extension: string;
+    url?: string;
+    imageClassName?: string;
+    className?: string;
+}
+
+export default function Thumbnail({ type, extension, url, imageClassName,
+    className }: IProps) {
+        const isImage = type === "image" && extension !== "svg";
+    return (
+        <figure className={cn('thumbnail', className)}>
+            <Image 
+            src={(isImage ? url : getFileIcon(extension, type)) as string}
+            alt='Thumbnail'
+            height={100}
+            width={100}
+            className={cn(
+                "size-8 object-contain",
+                imageClassName,
+                isImage && "thumbnail-image"
+            )}
+            />
+        </figure>
+    )
+}
