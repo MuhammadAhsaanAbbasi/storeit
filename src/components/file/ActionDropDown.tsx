cb5ba3ef382b4540bfcb61a384f38583192186fd
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2025-08-05 22:13:21 +0500

    Action DropDown

--- src/components/file/ActionDropDown.tsx
+++ src/components/file/ActionDropDown.tsx
@@ -1,17 +1,62 @@
+"use client";
 import Image from 'next/image';
 import { Models } from 'node-appwrite';
-import React from 'react'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import React, { useState } from 'react';
+import { actionsDropdownItems } from '@/constants';
+import { Dialog } from '../ui/dialog';
 
-const ActionDropDown = ({file}: {file: Models.Document}) => {
+const ActionDropDown = ({ file }: { file: Models.Document }) => {
+    const [isDropDown, setIsDropDown] = useState(false);
+    const [isDialogOpen, setIsDialogOpen] = useState(false);
+    const [actions, setActions] = useState<ActionType | null>(null);
     return (
-        <div>
-            <Image 
-            src={"/icons/dots.svg"}
-            alt="dots"
-            width={34}
-            height={34}
-            />
-        </div>
+        <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+            <DropdownMenu open={isDropDown} onOpenChange={setIsDropDown}>
+                <DropdownMenuTrigger className="shad-no-focus">
+                    <Image
+                        src={"/icons/dots.svg"}
+                        alt="dots"
+                        width={34}
+                        height={34}
+                    />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <DropdownMenuLabel className="max-w-[200px] truncate">
+                        {file.name}
+                    </DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    {
+                        actionsDropdownItems.map((action) => (
+                            <DropdownMenuItem key={action.value}
+                                className='shad-dropdown-item'
+                                onClick={() => {
+                                    setActions(action);
+
+                                    if (
+                                        ["rename", "share", "delete", "details"].includes(
+                                            action.value,
+                                        )
+                                    ) {
+                                        setIsDialogOpen(true);
+                                    }
+
+                                }}
+                            >
+                                {action.label}
+                            </DropdownMenuItem>
+                        ))
+                    }
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </Dialog>
     )
 }
 
