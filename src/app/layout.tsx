5fa2ba750993178254defd65d298c20a2719a4ef
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2025-08-13 10:28:45 +0500

    Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import {  Inter, Poppins } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from "@vercel/analytics/next"
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -32,6 +33,7 @@ export default function MainRootLayout({
       >
         {children}
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
